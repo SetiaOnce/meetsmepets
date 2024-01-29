724a2b6d7ae9bff575d9122cddc339b1efce4e0a
SetiaOnce <gedeyoga1126@gmail.com>
2024-01-29 18:48:54 +0700

    Create profile_edit.blade.php

---
+++ resources/views/frontend/profile_edit.blade.php
@@ -0,0 +1,142 @@
+@extends('frontend.layouts', ['activeMenu' => 'PROFILE', 'activeSubMenu' => ''])
+@section('content')
+<!-- Page Content Start -->
+<div class="page-content space-top">
+    <div class="container"> 
+        <div class="card style-3">
+            <div class="card-header">
+                <h6 class="title mb-0 font-14 font-w500">Full Name</h6>
+            </div>
+            <div class="card-body">
+                <a href="javascript:void(0);" class="setting-input" data-bs-toggle="offcanvas" data-bs-target="#offcanvasBottom1" aria-controls="offcanvasBottom">
+                    <i class="icon dz-flex-box feather icon-phone-call"></i>
+                    <span class="full_name">{{ $data['user_session']['name'] }}</span>
+                </a>
+            </div>
+        </div>
+        <div class="card style-3">
+            <div class="card-header">
+                <h6 class="title mb-0 font-14 font-w500">Your Foto Profiles <br><p class="text-muted">*) Click to update image | max : <code>2MB</code> | mimes : <code>jpg, jpeg, png</code>
+                </p></h6>
+            </div>
+            <div class="card-body justify-content-center">
+                <form class="form" id="form-fotoProfiles">
+                    <!--begin::Input group-->
+                    <div class="mb-3" id="iGroup-foto_profiles">
+                        <input type="file" class="dropify-upl mb-3 mb-lg-0" id="foto_profiles" name="foto_profiles" accept=".png, .jpg, .jpeg" data-show-remove="false" data-allowed-file-extensions="jpg png jpeg" data-max-file-size="2M" />
+                    </div>
+                    <!--end::Input group-->
+                </form>
+            </div>
+        </div>
+        <form class="form" id="form-images">
+            <div class="row g-3 mb-3"  data-masonry='{"percentPosition": true }'>
+            <div class="card-header">
+                <h6 class="title mb-0 font-14 font-w500">Your Pets Album <br><p class="text-muted">*) Click to add or update image | max : <code>2MB</code> | mimes : <code>jpg, jpeg, png</code>
+                </p></h6>
+            </div>
+                <div class="col-8">
+                    <div class="dz-drop-box style-2">
+                        <div class="drop-bx bx-lg">
+                            <div class="imagePreview imagePreview1" style="background-image: url({{ asset('dist/assets/images/recent-pic/drop-bx.png') }});">
+                                <div  class="remove-img remove-btn d-none remove-btn1"><i class="icon feather icon-x"></i></div>
+                                <input type='file' class="form-control d-none imageUpload" name="imageUpload" id="imageUpload" accept=".png, .jpg, .jpeg">
+                                <label for="imageUpload"></label>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-4">
+                    <div class="row g-3">
+                        <div class="col-12">
+                            <div class="dz-drop-box style-2">
+                                <img src="{{ asset('dist/assets/images/recent-pic/drop-bx.png') }}" alt="">
+                                <div class="drop-bx">
+                                    <div class="imagePreview imagePreview2" style="background-image: url({{ asset('dist/assets/images/recent-pic/drop-bx.png') }});">
+                                        <div  class="remove-img remove-btn d-none remove-btn2"><i class="icon feather icon-x"></i></div>
+                                        <input type='file' class="form-control d-none imageUpload" name="imageUpload2"  id="imageUpload2" accept=".png, .jpg, .jpeg">
+                                        <label for="imageUpload2"></label>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-12">
+                            <div class="dz-drop-box style-2">
+                                <img src="{{ asset('dist/assets/images/recent-pic/drop-bx.png') }}" alt="">
+                                <div class="drop-bx">
+                                    <div class="imagePreview imagePreview3" style="background-image: url({{ asset('dist/assets/images/recent-pic/drop-bx.png') }});">
+                                        <div  class="remove-img remove-btn d-none remove-btn3"><i class="icon feather icon-x"></i></div>
+                                        <input type='file' class="form-control d-none imageUpload" name="imageUpload3"  id="imageUpload3" accept=".png, .jpg, .jpeg">
+                                        <label for="imageUpload3"></label>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-4">
+                    <div class="dz-drop-box style-2">
+                        <img src="{{ asset('dist/assets/images/recent-pic/drop-bx.png') }}" alt="">
+                        <div class="drop-bx">
+                            <div class="imagePreview imagePreview4" style="background-image: url({{ asset('dist/assets/images/recent-pic/drop-bx.png') }});">
+                                <div  class="remove-img remove-btn d-none remove-btn4"><i class="icon feather icon-x"></i></div>
+                                <input type='file' class="form-control d-none imageUpload" name="imageUpload4" id="imageUpload4" accept=".png, .jpg, .jpeg">
+                                <label for="imageUpload4"></label>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-4">
+                    <div class="dz-drop-box style-2">
+                        <img src="{{ asset('dist/assets/images/recent-pic/drop-bx.png') }}" alt="">
+                        <div class="drop-bx">
+                            <div class="imagePreview imagePreview5" style="background-image: url({{ asset('dist/assets/images/recent-pic/drop-bx.png') }});">
+                                <div  class="remove-img remove-btn d-none remove-btn5"><i class="icon feather icon-x"></i></div>
+                                <input type='file' class="form-control d-none imageUpload" name="imageUpload5" id="imageUpload5" accept=".png, .jpg, .jpeg">
+                                <label for="imageUpload5"></label>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-4">
+                    <div class="dz-drop-box style-2">
+                        <img src="{{ asset('dist/assets/images/recent-pic/drop-bx.png') }}" alt="">
+                        <div class="drop-bx">
+                            <div class="imagePreview imagePreview6" style="background-image: url({{ asset('dist/assets/images/recent-pic/drop-bx.png') }});">
+                                <div  class="remove-img remove-btn d-none remove-btn6"><i class="icon feather icon-x"></i></div>
+                                <input type='file' class="form-control d-none imageUpload"  id="imageUpload6" name="imageUpload6" accept=".png, .jpg, .jpeg">
+                                <label for="imageUpload6"></label>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </form>
+        <div class="card style-3 pb-5">
+            <div class="card-header">
+                <h6 class="title mb-0 font-14 font-w500">Interest</h6>
+            </div>
+            <div class="card-body">
+                <select class="form-control selectpicker form-control-solid mb-3 mb-lg-0" id="interest" name="interest[]" multiple data-placeholder="Select here"></select>
+            </div>
+        </div>
+    </div> 
+</div>
+<!-- Full Name OffCanvas -->
+<div class="offcanvas offcanvas-bottom" tabindex="-1" id="offcanvasBottom1">
+    <button type="button" class="btn-close drage-close" data-bs-dismiss="offcanvas" aria-label="Close"></button>
+    <div class="offcanvas-header share-style m-0 pb-0">
+        <h6 class="title">Your Full Name</h6>
+    </div>
+    <div class="offcanvas-body overflow-visible">
+        <form>
+            <div class="input-group dz-select">
+                <input type="text" name="full_name" id="full_name" class="form-control" value="{{ $data['user_session']['name'] }}">
+            </div>
+            <a href="javascript:void(0);" id="btn-name" class="btn btn-gradient w-100 dz-flex-box btn-shadow rounded-xl" data-bs-dismiss="offcanvas" aria-label="Close">Save</a>
+        </form>		
+    </div>
+</div>
+<!-- Full Name OffCanvas -->
+<!-- Page Content End -->
+@endsection
