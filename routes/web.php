2cb546113685a0534437701223792e4e96a675c3
SetiaOnce <gedeyoga1126@gmail.com>
2024-01-29 18:48:58 +0700

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\CommonController;
+use App\Http\Controllers\SelectController;
 use App\Http\Controllers\Welcome\LoginController;
 use App\Http\Controllers\Welcome\RegisterController;
 use App\Http\Controllers\Welcome\WelcomeController;
@@ -18,6 +19,13 @@ Route::group(['prefix' => 'api'], function () {
     Route::controller(CommonController::class)->group(function(){
         Route::get('/site_info', 'siteInfo');
         Route::get('/user_info', 'userInfo');
+        Route::get('/owner_info', 'ownerInfo');
+    });
+    Route::controller(SelectController::class)->group(function(){
+        Route::group(['prefix' => 'select'], function () {
+            Route::get('/looking_for', 'lookingFor');
+            Route::get('/interest', 'interest');
+        });
     });
     Route::controller(AuthController::class)->group(function () {
         Route::group(['prefix' => 'auth'], function () {
@@ -30,6 +38,7 @@ Route::group(['prefix' => 'api'], function () {
 Route::get('/', [WelcomeController::class, 'index'])->name('welcome');
 Route::controller(LoginController::class)->group(function(){
     Route::get('/auth', 'index');
+    Route::get('/logout', 'logout_sessions');
     Route::group(['prefix' => 'api'], function () {
         Route::post('auth/first_step', 'first_step')->name('first_step');
         Route::post('auth/second_step', 'second_step')->name('second_step');
