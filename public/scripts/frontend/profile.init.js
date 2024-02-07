fb8beeafd9b908e1121f48bc17c2d18ee9ea9bb8
SetiaOnce <gedeyoga1126@gmail.com>
2024-02-07 10:26:08 +0700

    Update profile.init.js

--- public/scripts/frontend/profile.init.js
+++ public/scripts/frontend/profile.init.js
@@ -8,7 +8,7 @@ const _loadOwnerInfo = () => {
             let userInfo = data.row;
             let AvatarUser = `<img src="` +userInfo.thumb_url+ `" alt="avatar-user">`;
             let profileDetail = `<h5 class="name">`+userInfo.name+`</h5>
-            <p class="mb-0"><i class="icon feather icon-map-pin me-1"></i>`+userInfo.location+`</p>`;
+            <p class="mb-0"><i class="icon feather icon-map-pin me-1"></i>`+(userInfo.location == null) ? '-':userInfo.location+`</p>`;
             $('#widgetOwner .avatar-user').html(AvatarUser);
             $('#widgetOwner .profile-detail').html(profileDetail);
         }, error: function (jqXHR, textStatus, errorThrown) {
