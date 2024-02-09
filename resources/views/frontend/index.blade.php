a0fca82ed896baeb2b1cd7fb165ee11dc582a0a1
SetiaOnce <gedeyoga1126@gmail.com>
2024-02-09 14:13:42 +0700

    Update index.blade.php

--- resources/views/frontend/index.blade.php
+++ resources/views/frontend/index.blade.php
@@ -1,6 +1,14 @@
 @extends('frontend.layouts', ['activeMenu' => 'HOME', 'activeSubMenu' => ''])
 @section('content')
-
-
-
+<!-- Page Content Start -->
+<div class="page-content space-top p-b65">
+    <div class="container fixed-full-area">
+        <div class="dzSwipe_card-cont dz-gallery-slider" id="sectionNearbyowners">
+            
+        </div>
+    </div>
+</div>
+<!-- Page Content End -->
+<input type="hidden" name="lat" id="lat">
+<input type="hidden" name="lng" id="lng">
 @endsection
