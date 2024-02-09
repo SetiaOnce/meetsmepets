c86be79be14ebb81aa4c6a73f4afa4b13d6f4865
SetiaOnce <gedeyoga1126@gmail.com>
2024-02-09 11:14:58 +0700

    Update profile_setting.init.js

--- public/scripts/frontend/profile_setting.init.js
+++ public/scripts/frontend/profile_setting.init.js
@@ -12,6 +12,12 @@ const _loadOwnerProfile = () => {
             $('#ktSettingProfile .email').html(userInfo.email);
             $('#ktSettingProfile .location').html(userInfo.location);
             $('#ktSettingProfile .gender').html((userInfo.gender == null)? 'OTHER':userInfo.gender);
+            // location
+            if(userInfo.lat != null && userInfo.lng != null){
+                mapsViews(userInfo.lat, userInfo.lng);
+            }else{
+                mapsViews(-6.175384188330963, 106.82715234249756);
+            }
             var lookingFor = '';
             if(userInfo.looking_for){
                 var lookingFor = userInfo.looking_for.split(", ");
@@ -157,41 +163,31 @@ $('#btn-email').on('click', function (e) {
     });
 });
 // save location address
-$('#btn-location').on('click', function (e) {
-    e.preventDefault();
-    $('#btn-location').html('Please Wait...').attr('disabled', true);
-    var location = $('#location');
-    if (location.val() == '') {
-        toastr.error('Enter your location address', 'Uuppss!', {"progressBar": true, "timeOut": 1500});
-        location.focus();
-        $('#btn-location').html('Next').attr('disabled', false);
-        return false;
-    }
-    location = location.val()
+const saveLocation = () => {
+    let location = $('#address'), lat = $('#lat'), lng = $('#lng');
+    location = location.val(), lat = lat.val(), lng = lng.val()
     $.ajax({
         url: base_url+ "api/profile/save_location",
         headers: { 'X-CSRF-Token': $('meta[name="csrf-token"]').attr('content') },
         type: "POST",
         data: {
-            location,
+            location,lat, lng
         },
         dataType: "JSON",
         success: function (data) {
-            $('#btn-location').html('Next').attr('disabled', false);
             if (data.status==true){
-                _loadOwnerProfile();
+                // _loadOwnerProfile();
             }else{
                 toastr.error(data.message, 'Ooops!', {"progressBar": true, "timeOut": 2500});
             }
         },
         error: function (jqXHR, textStatus, errorThrown) {
-            $('#btn-location').html('Next').attr('disabled', false);
             Swal.fire({title: "Ooops!", text: "Terjadi kesalahan yang tidak diketahui, mohon hubungi pengembang!", icon: "error", allowOutsideClick: false}).then(function (result) {
                 location.reload(true);
             });
         }
     });
-});
+}
 // save gender
 const container = document.querySelector('#offcanvasBottom4');
 const checkboxes = container.querySelectorAll('[name="gender"]');
@@ -302,6 +298,69 @@ $('#looking_for').change(function(){
         }
     });
 });
+// maps location view
+const mapsViews = (location_Lat, location_lng) => {
+    const coordinateView = [location_Lat,location_lng];
+    let maps = L.map('mapsViewLocation', {
+        preferCanvas: true,
+        minZoom: 5,
+        zoomControl: false,
+        attributes: true
+    }).setView(coordinateView, 8);
+    L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token=' +mapbox_accessToken, {
+        maxZoom: 16,
+        tileSize: 512,
+        zoomOffset: -1,
+        id: 'mapbox/streets-v11'
+    }).addTo(maps);
+
+    let latCoords = parseFloat(location_Lat),
+    lngCoords = parseFloat(location_lng);
+    var infoIcon = L.icon({
+        iconUrl: base_url+ 'dist/img/pin.png',
+        iconSize: [64,64]
+    });
+    var marker = L.marker([latCoords,lngCoords],{
+        icon:infoIcon,
+        draggable: true
+    }).addTo(maps);
+
+    maps.on("click",function(e){
+        var lat=e.latlng.lat;
+        var lng=e.latlng.lng;
+        if(!marker){
+            marker = L.marker(e.latlng).addTo(map)
+        }else{
+            marker.setLatLng(e.latlng);
+        }
+        getAdrresFromKordinat(lat, lng, function(address) { 
+            $('[name="address"]').val(address), $('[name="lat"]').val(lat), $('[name="lng"]').val(lng);
+            saveLocation();
+        });
+    });
+}
+// get location from lat lng
+const getAdrresFromKordinat = (lat, lng, callback) => {
+    var geocodingUrl = 'https://maps.googleapis.com/maps/api/geocode/json?latlng=' + lat + ',' + lng + '&key=AIzaSyCXSESO13hs0eNGVv9_Q8Ynbf0NcU4chIg';
+    // AJAX request
+    $.ajax({
+        url: geocodingUrl,
+        type: 'GET',
+        success: function(response) {
+            if(response.status == 'OK') {
+                // Extract address from response
+                var address = response.results[0].formatted_address;
+                callback(address);
+            } else {
+                callback('');
+            }
+        },
+        error: function(xhr, status, error) {
+            console.error('Error:', error);
+            callback('');
+        }
+    });
+}
 // Class Initialization
 jQuery(document).ready(function() {
     loadSelectpicker_lookingfor();
