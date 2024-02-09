e204922231c94e0222bef74061a56cbf85d0aece
SetiaOnce <gedeyoga1126@gmail.com>
2024-02-09 14:13:37 +0700

    Update HomeController.php

--- app/Http/Controllers/Frontend/HomeController.php
+++ app/Http/Controllers/Frontend/HomeController.php
@@ -2,8 +2,11 @@
 
 namespace App\Http\Controllers\Frontend;
 
+use App\Helpers\Shortcut;
 use App\Http\Controllers\Controller;
+use App\Models\Owners;
 use App\Models\SiteInfo;
+use App\Models\SuperLike;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 
@@ -23,12 +26,74 @@ class HomeController extends Controller
         );
         //Data Source CSS
         $data['css'] = array(
+            'dist/assets/vendor/bootstrap-touchspin/dist/jquery.bootstrap-touchspin.min.css',
+            'dist/assets/vendor/nouislider/nouislider.min.css',
+            'dist/assets/vendor/swiper/swiper-bundle.min.css',
         );
         //Data Source JS
         $data['js'] = array(
+            'dist/assets/vendor/swiper/swiper-bundle.min.js',
+            'dist/assets/vendor/bootstrap-touchspin/dist/jquery.bootstrap-touchspin.min.js',
+            'dist/assets/js/tinderSwiper.min.js',
+            'dist/assets/vendor/wnumb/wNumb.js',
+            'dist/assets/vendor/nouislider/nouislider.min.js',
+            'dist/assets/js/noui-slider.init.js',
+            'dist/assets/js/dz.carousel.js',
             'scripts/frontend/siteinfo.init.js',
             'scripts/frontend/home.init.js',
         );
         return view('frontend.index', compact('data'));
+    }
+    public function nearOwner(Request $request)
+    {
+        $iduserSesIdp = Auth::guard('owner')->user()->id;  
+        $maxDistance = Auth::guard('owner')->user()->distance;  
+        if($request->lat AND $request->lat){
+            $lat = $request->lat;
+            $lng = $request->lng;  
+        }else{
+            $lat =Auth::guard('owner')->user()->lat;
+            $lng =Auth::guard('owner')->user()->lng;  
+        }
+        $getRow =  Owners::whereNot('id', $iduserSesIdp)->whereIsActive('Y')->get();
+        $dataNearby = [];
+        foreach ($getRow as $item) {
+            $distance = $this->haversineDistance($lat, $lng, $item->lat, $item->lng);
+            if ($distance <= $maxDistance) {
+                $file_name = $item->thumb;
+                if($file_name==''){
+                    $thumb_url = asset('dist/img/default-user-img.jpg');
+                } else {
+                    if (!file_exists(public_path(). '/dist/img/users-img/'.$file_name)){
+                        $thumb_url = asset('dist/img/default-user-img.jpg');
+                    }else{
+                        $thumb_url = url('dist/img/users-img/'.$file_name);
+                    }
+                }
+                $statusLike = SuperLike::whereFidOwner($item->id)->whereLikeBy($iduserSesIdp)->first();
+                $dataNearby[] = [
+                    'id' => $item->id,
+                    'name' => $item->name,
+                    'distance' => number_format($distance, 2),
+                    'thumb_url' => $thumb_url,
+                    'has_like' => ($statusLike == null)? 'N' : 'Y'
+                ];
+            }
+        }
+        return Shortcut::jsonResponse(true, 'Success', 200, $dataNearby);
+    }
+    // Fungsi untuk menghitung jarak antara dua titik pada bola bumi menggunakan formula Haversine
+    protected function haversineDistance($lat1, $lon1, $lat2, $lon2) {
+        $earthRadius = 6371; // radius of the Earth in kilometers
+
+        $deltaLat = deg2rad($lat2 - $lat1);
+        $deltaLon = deg2rad($lon2 - $lon1);
+
+        $a = sin($deltaLat / 2) * sin($deltaLat / 2) + cos(deg2rad($lat1)) * cos(deg2rad($lat2)) * sin($deltaLon / 2) * sin($deltaLon / 2);
+        $c = 2 * atan2(sqrt($a), sqrt(1 - $a));
+
+        $distance = $earthRadius * $c; // Distance in kilometers
+
+        return $distance;
     }
 }
