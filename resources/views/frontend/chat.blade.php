39ecaf6afc191898abc2dcff32708e4ad6603226
SetiaOnce <gedeyoga1126@gmail.com>
2024-02-09 21:39:45 +0700

    Create chat.blade.php

---
+++ resources/views/frontend/chat.blade.php
@@ -0,0 +1,18 @@
+@extends('frontend.layouts', ['activeMenu' => 'CHAT', 'activeSubMenu' => ''])
+@section('content')
+<!-- Page Content Start -->
+<div class="page-content space-top p-b60" id="sectionChat">
+    <div class="container">
+        <div class="swiper chat-swiper">
+            <div class="swiper-wrapper owner-love"></div>
+        </div>
+        <div class="title-bar">
+            <h6 class="title">Message</h6>
+        </div>
+        <ul class="message-list all-message">
+
+        </ul>
+    </div>    
+</div>
+<!-- Page Content End -->
+@endsection
