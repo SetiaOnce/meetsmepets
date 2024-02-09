23b0e8bc0a091247211402c6455e02ffd0ad1c5c
SetiaOnce <gedeyoga1126@gmail.com>
2024-02-09 21:39:29 +0700

    Create StatusOnline.php

---
+++ app/Events/StatusOnline.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Events;
+
+use Illuminate\Broadcasting\Channel;
+use Illuminate\Broadcasting\InteractsWithSockets;
+use Illuminate\Contracts\Broadcasting\ShouldBroadcast;
+use Illuminate\Foundation\Events\Dispatchable;
+use Illuminate\Queue\SerializesModels;
+
+class StatusOnline implements ShouldBroadcast
+{
+    use Dispatchable, InteractsWithSockets, SerializesModels;
+
+    public $users;
+    public $message;
+
+    /**
+     * Create a new event instance.
+     *
+     * @return void
+     */
+    public function __construct($user, $statusOnline)
+    {
+        $this->users = $user;
+        $this->message  = "{$user} sedang {$statusOnline}";
+    }
+
+    /**
+     * Get the channels the event should broadcast on.
+     *
+     * @return \Illuminate\Broadcasting\Channel|array
+     */
+    public function broadcastOn()
+    {
+        return new Channel('status-online');
+    }
+}
