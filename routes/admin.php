8e6c8e60882dd079dc3c04c7ddf910dee0fc5a51
SetiaOnce <gedeyoga1126@gmail.com>
2024-02-15 16:59:35 +0700

    Update admin.php

--- routes/admin.php
+++ routes/admin.php
@@ -2,6 +2,8 @@
 
 use App\Http\Controllers\Backend\WebsitesInfomationController;
 use App\Http\Controllers\Backend\DashboardController;
+use App\Http\Controllers\Backend\MasterCategoryController;
+use App\Http\Controllers\Backend\MasterInterestController;
 use App\Http\Controllers\Backend\UserProfilesController;
 use App\Http\Controllers\Backend\UsersSystemController;
 use Illuminate\Support\Facades\Route;
@@ -12,6 +14,11 @@ Route::get('/site_information', [WebsitesInfomationController::class, 'index']);
 Route::group(['prefix' => 'app_admin'], function () {
     Route::get('/manage_users', [UsersSystemController::class,'index'])->name('manage_users');
     Route::get('/{username}', [UserProfilesController::class,'index'])->name('user_profile');
+});
+// Mastering
+Route::group(['prefix' => 'master'], function () {
+    Route::get('/category', [MasterCategoryController::class,'index'])->name('master_category');
+    Route::get('/interest', [MasterInterestController::class,'index'])->name('master_interest');
 });
 //Api Ajax App
 Route::group(['prefix' => 'api'], function () {
@@ -34,4 +41,22 @@ Route::group(['prefix' => 'api'], function () {
     Route::post('/manage_users/update', [UsersSystemController::class, 'update'])->name('update_users');
     Route::post('/manage_users/update_statususers', [UsersSystemController::class, 'update_statususers'])->name('update_statususers');
     Route::post('/manage_users/reset_userpass', [UsersSystemController::class, 'reset_userpass'])->name('reset_userpass');
+    // master category
+    Route::controller(MasterCategoryController::class)->group(function () {
+        Route::group(['prefix' => 'master_category'], function () {
+            Route::get('show', 'show')->name('show_category');
+            Route::post('store', 'store')->name('store_category');
+            Route::post('update', 'update')->name('update_category');
+            Route::post('update_status', 'update_status')->name('update_status_category');
+        });
+    });
+    // master interest
+    Route::controller(MasterInterestController::class)->group(function () {
+        Route::group(['prefix' => 'master_interest'], function () {
+            Route::get('show', 'show')->name('show_interest');
+            Route::post('store', 'store')->name('store_interest');
+            Route::post('update', 'update')->name('update_interest');
+            Route::post('update_status', 'update_status')->name('update_status_interest');
+        });
+    });
 });
