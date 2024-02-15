f955e1275be1ee8943cac8ab2b6c6d34111828b7
SetiaOnce <gedeyoga1126@gmail.com>
2024-02-15 16:59:21 +0700

    Create master_category.blade.php

---
+++ resources/views/backend/master_category.blade.php
@@ -0,0 +1,106 @@
+@extends('backend.layouts', ['activeMenu' => 'MASTER_CATEGORY', 'activeSubMenu' => 'master'])
+@section('content')
+<!--begin::Card Form-->
+<div class="card" id="card-form" style="display: none;">
+    <!--begin::Card header-->
+    <div class="card-header">
+        <div class="card-title"></div>
+        <div class="card-toolbar">
+            <div class="d-flex justify-content-end">
+                <button type="button" class="btn btn-sm btn btn-bg-light btn-color-danger me-2" id="btn-closeForm" onclick="_closeCard('form_category');"><i class="fas fa-times"></i> Tutup</button>
+            </div>
+        </div>
+    </div>
+    <!--end::Card header-->
+    <!--begin::Form-->
+    <form id="form-data" class="form" onsubmit="return false">
+        <input type="hidden" name="id" /><input type="hidden" name="method_formdata" />
+        <!--begin::Card body-->
+        <div class="card-body">
+            <!--begin::Row-->
+            <div class="row">
+                <div class="col-md-12">
+                    <!--begin::Input group-->
+                    <div class="mb-3">
+                        <!--begin::Input group-->
+                        <div class="mb-3">
+                            <label class="col-form-label required fw-bold fs-6" for="category_name">Category Name</label>
+                            <input type="text" class="form-control form-control-solid" name="category_name" id="category_name" maxlength="120" placeholder="Isikan nama category ..." />
+                        </div>
+                        <!--end::Input group-->
+                        <!--begin::Input group-->
+                        <div class="mb-3" id="iGroup-isActive" style="display: none;">
+                            <label class="col-form-label fw-bold fs-6" for="is_active">Status</label>
+                            <div class="form-check form-switch form-check-custom form-check-solid">
+                                <input class="form-check-input" type="checkbox" id="is_active" name="is_active" />
+                                <label class="form-check-label" for="is_active"></label>
+                            </div>
+                            <div class="form-text">*) Jika <code>Tidak Aktif</code>, tidak bisa digunakan</div>
+                        </div>
+                        <!--end::Input group-->
+                    </div>
+                </div>
+            </div>
+            <!--end::Row-->
+        </div>
+        <!--end::Card body-->
+        <!--begin::Actions-->
+        <div class="card-footer d-flex justify-content-end py-6 px-9">
+            <button type="button" class="btn btn-light btn-active-light-danger me-2" id="btn-reset" onclick="_clearForm();"><i class="las la-redo-alt fs-1 me-3"></i>Batal</button>
+            <button type="button" class="btn btn-primary" id="btn-save"><i class="las la-save fs-1 me-3"></i>Simpan</button>
+        </div>
+        <!--end::Actions-->
+    </form>
+    <!--end::Form-->
+</div>
+<!--end::Card Form-->
+<!--begin::List Table Data-->
+<div class="card shadow" id="card-data">
+    <!--begin::Card header-->
+    <div class="card-header">
+        <div class="card-title">
+            <h3 class="fw-bolder fs-2 text-gray-900">
+                <i class="fas fa-th-list fs-2 text-gray-900 me-2"></i> Data Category
+            </h3>
+        </div>
+        <div class="card-toolbar">
+            <div class="d-flex justify-content-end">
+                <button type="button" class="btn btn-sm btn-primary me-2" id="btn-addData" onclick="_addData();"><i class="las la-plus fs-3"></i> Tambah</button>
+            </div>
+        </div>
+    </div>
+    <!--end::Card header-->
+    <div class="card-body">
+        <div class="d-flex flex-wrap justify-content-center align-items-center mb-5">
+            <div class="ms-auto">
+                <!--begin::Search-->
+                <div class="d-flex align-items-center position-relative mb-md-0 mb-3">
+                    <div class="input-group input-group-sm input-group-solid border">
+                        <span class="input-group-text"><i class="las la-search fs-3"></i></span>
+                        <input type="text" class="form-control form-control-sm form-control-solid border-left-0" name="search-DtCategory" id="search-DtCategory" placeholder="Pencarian..." />
+                        <span class="input-group-text border-left-0 cursor-pointer text-hover-danger" id="clear-searchDtCategory" style="display: none;">
+                            <i class="las la-times fs-3"></i>
+                        </span>
+                    </div>
+                </div>
+                <!--end::Search-->
+            </div>
+        </div>
+        <div class="table-responsive">
+            <!--begin::Table-->
+            <table class="table table-rounded align-middle table-row-bordered border" id="dt-category">
+                <thead>
+                    <tr class="fw-bolder text-uppercase bg-light">
+                        <th class="text-center align-middle px-2 border-bottom-2 border-gray-200">No.</th>
+                        <th class="align-middle px-2 border-bottom-2 border-gray-200">Category Name</th>
+                        <th class="align-middle px-2 border-bottom-2 border-gray-200">Active</th>
+                        <th class="text-center align-middle px-2 border-bottom-2 border-gray-200">Aksi</th>
+                    </tr>
+                </thead>
+            </table>
+            <!--end::Table-->
+        </div>
+    </div>
+</div>
+<!--end::List Table Data-->
+@endsection
