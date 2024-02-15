a09c440c66f1e5aed3c4d13900789177aa658ba5
SetiaOnce <gedeyoga1126@gmail.com>
2024-02-15 16:58:53 +0700

    Update SelectController.php

--- app/Http/Controllers/SelectController.php
+++ app/Http/Controllers/SelectController.php
@@ -4,74 +4,52 @@ namespace App\Http\Controllers;
 
 use App\Helpers\Shortcut;
 use App\Http\Controllers\Controller;
+use App\Models\MasterCategory;
+use App\Models\MasterInterest;
+use App\Models\Pets;
 use Illuminate\Http\Request;
 
 class SelectController extends Controller
 {
     public function lookingFor(Request $request)
     {
-        $getRow = [
-            [
-                'id' => 'CAT',
-                'text' => 'CAT',
-            ],
-            [
-                'id' => 'DOG',
-                'text' => 'DOG',
-            ],
-            [
-                'id' => 'BIRD',
-                'text' => 'BIRD',
-            ],
-            [
-                'id' => 'FISH',
-                'text' => 'FISH',
-            ],
-        ];
-        return Shortcut::jsonResponse(true, 'Success', 200, $getRow);
+        $getRow = MasterInterest::orderByDesc('id')->whereIsActive('Y')->get();
+        $data = [];
+        foreach($getRow as $row){            
+            $data[] = [
+                'id' => $row->interest,
+                'text' => $row->interest,
+            ];
+        }
+        return Shortcut::jsonResponse(true, 'Success', 200, $data);
     }
     public function interest(Request $request)
     {
-        $getRow = [
-            [
-                'id' => 'CAT',
-                'text' => 'CAT',
-            ],
-            [
-                'id' => 'DOG',
-                'text' => 'DOG',
-            ],
-            [
-                'id' => 'BIRD',
-                'text' => 'BIRD',
-            ],
-            [
-                'id' => 'FISH',
-                'text' => 'FISH',
-            ],
-        ];
-        return Shortcut::jsonResponse(true, 'Success', 200, $getRow);
+        $getRow = MasterInterest::orderByDesc('id')->whereIsActive('Y')->get();
+        $data = [];
+        foreach($getRow as $row){            
+            $data[] = [
+                'id' => $row->interest,
+                'text' => $row->interest,
+            ];
+        }
+        return Shortcut::jsonResponse(true, 'Success', 200, $data);
     }
     public function category(Request $request)
     {
-        $getRow = [
-            [
-                'id' => 'CAT',
-                'text' => 'Cat',
-            ],
-            [
-                'id' => 'DOG',
-                'text' => 'Dog',
-            ],
-            [
-                'id' => 'BIRD',
-                'text' => 'Bird',
-            ],
-            [
-                'id' => 'FISH',
-                'text' => 'Fish',
-            ],
-        ];
+        $getRow = MasterCategory::orderByDesc('id')->whereIsActive('Y')->get();
+        $data = [];
+        foreach($getRow as $row){            
+            $data[] = [
+                'id' => $row->category,
+                'text' => $row->category,
+            ];
+        }
+        return Shortcut::jsonResponse(true, 'Success', 200, $data);
+    }
+    public function breed(Request $request)
+    {
+        $getRow = Pets::whereIn('category', $request->category)->get(['id', 'breed']);
         return Shortcut::jsonResponse(true, 'Success', 200, $getRow);
     }
 }
