ad7388fe494a8cc19bbdace92a29a0a6e6df037d
SetiaOnce <gedeyoga1126@gmail.com>
2024-02-15 16:59:11 +0700

    Create master_interest.init.js

---
+++ public/scripts/backend/master_interest.init.js
@@ -0,0 +1,299 @@
+"use strict";
+//Class Definition
+var save_method;
+var table;
+//Load Datatables Interest
+const _loadDtInterest = () => {
+    table = $('#dt-interest').DataTable({
+        searchDelay: 300,
+        processing: true,
+        serverSide: true,
+        ajax: {
+            url: base_url+ 'api/master_interest/show',
+            headers: { 'X-CSRF-Token': $('meta[name="csrf-token"]').attr('content') },
+            type: 'GET',
+        },
+        destroy: true,
+        draw: true,
+        deferRender: true,
+        responsive: false,
+        autoWidth: false,
+        LengthChange: true,
+        paginate: true,
+        pageResize: true,
+        order: [[ 1, 'asc' ]],
+        columns: [
+            { data: 'DT_RowIndex', name: 'DT_RowIndex', width: "5%", className: "align-top text-center border px-2", searchable: false },
+            { data: 'interest', name: 'interest', width: "30%", className: "align-top border px-2" },
+            { data: 'is_active', name: 'is_active', width: "15%", className: "align-top text-center border px-2" },
+            { data: 'action', name: 'action', width: "15%", className: "align-top text-center border px-2", orderable: false, searchable: false },
+        ],
+        oLanguage: {
+            sEmptyTable: "Tidak ada Data yang dapat ditampilkan..",
+            sInfo: "Menampilkan _START_ s/d _END_ dari _TOTAL_",
+            sInfoEmpty: "Menampilkan 0 - 0 dari 0 entri.",
+            sInfoFiltered: "",
+            sProcessing: `<div class="d-flex justify-content-center align-items-center"><span class="spinner-border align-middle me-3"></span> Mohon Tunggu...</div>`,
+            sZeroRecords: "Tidak ada Data yang dapat ditampilkan..",
+            sLengthMenu: `<select class="mb-2 show-tick form-select-solid" data-width="fit" data-style="btn-sm btn-secondary" data-container="body">
+                <option value="10">10</option>
+                <option value="20">20</option>
+                <option value="30">30</option>
+                <option value="40">40</option>
+                <option value="50">50</option>
+                <option value="-1">Semua</option>
+            </select>`,
+            oPaginate: {
+                sPrevious: "Sebelumnya",
+                sNext: "Selanjutnya",
+            },
+        },
+        //"dom": "<'row'<'col-sm-6 d-flex align-items-center justify-conten-start'l><'col-sm-6 d-flex align-items-center justify-content-end'f>><'table-responsive'tr><'row'<'col-sm-12 col-md-5 d-flex align-items-center justify-content-center justify-content-md-start'i><'col-sm-12 col-md-7 d-flex align-items-center justify-content-center justify-content-md-end'p>>",
+        fnDrawCallback: function (settings, display) {
+            $('[data-bs-toggle="tooltip"]').tooltip("dispose"), $(".tooltip").hide();
+            //Search Table
+            $("#search-DtInterest").on("keyup", function () {
+                table.search(this.value).draw();
+                if ($(this).val().length > 0) {
+                    $("#clear-searchDtInterest").show();
+                } else {
+                    $("#clear-searchDtInterest").hide();
+                }
+            });
+            //Clear Search Table
+            $("#clear-searchDtInterest").on("click", function () {
+                $("#search-DtInterest").val(""),
+                table.search("").draw(),
+                $("#clear-searchDtInterest").hide();
+            });
+            //Custom Table
+            $("#dt-interest_length select").selectpicker(),
+            $('[data-bs-toggle="tooltip"]').tooltip({ 
+                trigger: "hover"
+            }).on("click", function () {
+                $(this).tooltip("hide");
+            });
+            $('.image-popup').magnificPopup({
+                type: 'image', closeOnContentClick: true, closeBtnInside: false, fixedContentPos: true,
+                image: {
+                    verticalFit: true
+                }
+            });
+        },
+    });
+    $("#dt-interest").css("width", "100%"),
+    $("#search-DtInterest").val(""),
+    $("#clear-searchDtInterest").hide();
+}
+//Close Content Card by Open Method
+const _closeCard = (card) => {
+    if(card=='form_interest') {
+        save_method = '';
+        _clearForm(), $('#card-form .card-header .card-title').html('');
+    }
+    $('#card-form').hide(), $('#card-data').show();
+}
+//Clear Form data
+const _clearForm = () => {
+    if (save_method == "" || save_method == "add_data") {
+        $("#form-data")[0].reset(), $('[name="id"]').val("");
+        $('#iGroup-isActive').hide();
+    } else {
+        let idp = $('[name="id"]').val();
+        _editData(idp);
+    }
+}
+//Add data
+const _addData = () => {
+    save_method = "add_data";
+    _clearForm(),
+    $("#card-form .card-header .card-title").html(
+        `<h3 class="fw-bolder fs-2 text-gray-900"><i class="bi bi-window-plus fs-2 text-gray-900 me-2"></i>Form Tambah Interest</h3>`
+    ),
+    $("#card-data").hide(), $("#card-form").show();
+}
+//Edit Interest
+const _editData = (idp) => {
+    save_method = "update_data";
+    $('#form-data')[0].reset(), $('#iGroup-isActive').show();
+    let target = document.querySelector("#card-form"), blockUi = new KTBlockUI(target, { message: messageBlockUi });
+    blockUi.block(), blockUi.destroy();
+    //Ajax load from ajax
+    $.ajax({
+        url: base_url+ 'api/master_interest/show',
+        headers: { 'X-CSRF-Token': $('meta[name="csrf-token"]').attr('content') },
+        type: 'GET',
+        dataType: 'JSON',
+        data: {
+            idp,
+        },
+        success: function (data) {
+            blockUi.release(), blockUi.destroy();
+            if (data.status == true) {
+                $('[name="id"]').val(data.row.id),
+                $('#interest_name').val(data.row.interest);
+                if (data.row.is_active == 'Y') {
+                    $('#is_active').prop('checked', true),
+                    $('#iGroup-isActive .form-check-label').text('AKTIF');
+                } else {
+                    $('#is_active').prop('checked', false),
+                    $('#iGroup-isActive .form-check-label').text('TIDAK AKTIF');
+                }
+                $("#card-form .card-header .card-title").html(
+                    `<h3 class="fw-bolder fs-2 text-gray-900"><i class="bi bi-pencil-square fs-2 text-gray-900 me-2"></i>Form Edit Interest</h3>`
+                ),
+                $("#card-data").hide(), $("#card-form").show();
+            } else {
+                Swal.fire({title: "Ooops!", text: data.message, icon: "warning", allowOutsideClick: false});
+            }
+        }, error: function (jqXHR, textStatus, errorThrown) {
+            blockUi.release(), blockUi.destroy();
+            console.log("load data is error!");
+            Swal.fire({
+                title: "Ooops!",
+                text: "Terjadi kesalahan yang tidak diketahui, Periksa koneksi jaringan internet lalu coba kembali. Mohon hubungi pengembang jika masih mengalami masalah yang sama.",
+                icon: "error",
+                allowOutsideClick: false,
+            });
+        },
+    });
+}
+//Save Interest by Enter
+$("#form-data input").keyup(function (event) {
+    if (event.keyCode == 13 || event.key === "Enter") {
+        $("#btn-save").click();
+    }
+});
+//Save Interest Form
+$("#btn-save").on("click", function (e) {
+    e.preventDefault();
+    $("#btn-save").html('<span class="spinner-border spinner-border-sm align-middle me-3"></span> Mohon Tunggu...').attr("disabled", true);
+    let interest_name = $("#interest_name");
+
+    if (interest_name.val() == "") {
+        toastr.error("Nama interest masih kosong...", "Uuppss!", { progressBar: true, timeOut: 1500 });
+        interest_name.focus();
+        $("#btn-save").html('<i class="las la-save fs-1 me-3"></i>Simpan').attr("disabled", false);
+        return false;
+    }
+
+    let textConfirmSave = "Simpan perubahan data sekarang ?";
+    if (save_method == "add_data") {
+        textConfirmSave = "Tambahkan data sekarang ?";
+    }
+
+    Swal.fire({
+        title: "",
+        text: textConfirmSave,
+        icon: "question",
+        showCancelButton: true,
+        allowOutsideClick: false,
+        confirmButtonText: "Ya",
+        cancelButtonText: "Batal",
+    }).then((result) => {
+        if (result.value) {
+            let target = document.querySelector("#card-form"), blockUi = new KTBlockUI(target, { message: messageBlockUi });
+            blockUi.block(), blockUi.destroy();
+            let formData = new FormData($("#form-data")[0]), ajax_url = base_url+ "api/master_interest/store";
+            if(save_method == 'update_data') {
+                ajax_url = base_url+ "api/master_interest/update";
+            }
+            $.ajax({
+                url: ajax_url,
+                headers: { 'X-CSRF-Token': $('meta[name="csrf-token"]').attr('content') },
+                type: "POST",
+                data: formData,
+                contentType: false,
+                processData: false,
+                dataType: "JSON",
+                success: function (data) {
+                    $("#btn-save").html('<i class="las la-save fs-1 me-3"></i>Simpan').attr("disabled", false);
+                    blockUi.release(), blockUi.destroy();
+                    if (data.status == true) {
+                        Swal.fire({
+                            title: "Success!",
+                            text: data.message,
+                            icon: "success",
+                            allowOutsideClick: false,
+                        }).then(function (result) {
+                            _closeCard('form_interest'), _loadDtInterest();
+                        });
+                    } else {
+                        Swal.fire({
+                            title: "Ooops!",
+                            html: data.message,
+                            icon: "warning",
+                            allowOutsideClick: false,
+                        })
+                    }
+                }, error: function (jqXHR, textStatus, errorThrown) {
+                    $("#btn-save").html('<i class="las la-save fs-1 me-3"></i>Simpan').attr("disabled", false);
+                    blockUi.release(), blockUi.destroy();
+                    Swal.fire({
+                        title: "Ooops!",
+                        text: "Terjadi kesalahan yang tidak diketahui, Periksa koneksi jaringan internet lalu coba kembali. Mohon hubungi pengembang jika masih mengalami masalah yang sama.",
+                        icon: "error",
+                        allowOutsideClick: false,
+                    });
+                }
+            });
+        } else {
+            $("#btn-save").html('<i class="las la-save fs-1 me-3"></i>Simpan').attr("disabled", false);
+        }
+    });
+});
+//Update Status Data interest
+const _updateStatus = (idp, value) => {
+    let textLbl = 'Nonaktifkan';
+    if(value=='Y') {
+        textLbl = 'Aktifkan';
+    }
+    let textSwal = textLbl+ ' interest sekarang ?';
+    Swal.fire({
+        title: "",
+        html: textSwal,
+        icon: "question",
+        showCancelButton: true,
+        allowOutsideClick: false,
+        confirmButtonText: "Ya",
+        cancelButtonText: "Tidak, Batalkan!"
+    }).then(result => {
+        if (result.value) {
+            let target = document.querySelector('#card-data'), blockUi = new KTBlockUI(target, { message: messageBlockUi });
+            blockUi.block(), blockUi.destroy();
+            // Load Ajax
+            $.ajax({
+                url: base_url+ "api/master_interest/update_status",
+                headers: { 'X-CSRF-Token': $('meta[name="csrf-token"]').attr('content') },
+                type: "POST",
+                dataType: "JSON",
+                data: {
+                    idp, value
+                }, success: function (data) {
+                    blockUi.release(), blockUi.destroy();
+                    Swal.fire({ title: "Success!", html: data.message, icon: "success", allowOutsideClick: false }).then(function (result) {
+                        _loadDtInterest();
+                    });
+                }, error: function (jqXHR, textStatus, errorThrown) {
+                    blockUi.release(), blockUi.destroy();
+                    Swal.fire({ title: "Ooops!", text: "Terjadi kesalahan yang tidak diketahui, Periksa koneksi jaringan internet lalu coba kembali. Mohon hubungi pengembang jika masih mengalami masalah yang sama.", icon: "error", allowOutsideClick: false }).then(function (result) {
+                        console.log("Update data is error!");
+                        _loadDtInterest();
+                    });
+                }
+            });
+        }
+    });
+}
+//Class Initialization
+jQuery(document).ready(function() {
+    _loadDtInterest();
+    //Change Check Switch
+    $("#is_active").change(function() {
+        if(this.checked) {
+            $('#iGroup-isActive .form-check-label').text('AKTIF');
+        }else{
+            $('#iGroup-isActive .form-check-label').text('TIDAK AKTIF');
+        }
+    });
+});
