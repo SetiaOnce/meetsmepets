915d13a8bec499395c19f4fce845bf6616bd0121
SetiaOnce <gedeyoga1126@gmail.com>
2024-02-15 16:59:17 +0700

    Update start_chat.init.js

--- public/scripts/frontend/start_chat.init.js
+++ public/scripts/frontend/start_chat.init.js
@@ -7,6 +7,7 @@ const _loadPersonalChat = () => {
         data: {
             idp_owner
         },success: function (data) {
+            console.log(data);
             let dataChat = data.row; 
             let contenMessagePersonal = '';
             if(dataChat.length == 0){
@@ -16,12 +17,13 @@ const _loadPersonalChat = () => {
                     contenMessagePersonal += `<div class="chat-content `+row.is_me+`">
                         <div class="message-item mb-2">
                             <div class="bubble">`+row.message+`</div>    
-                            <div class="message-time">08:40</div>    
+                            <div class="message-time">`+row.timechat+`</div>    
                         </div>
                     </div>`;
                 });
             }
-            $('#sectionChat .chat-message').html(contenMessagePersonal);
+            $('#'+key_chat+'').html(contenMessagePersonal);
+            window.scrollTo(0, document.body.scrollHeight);
         }, error: function (jqXHR, textStatus, errorThrown) {
             console.log('Load data is error');
         }
@@ -30,18 +32,15 @@ const _loadPersonalChat = () => {
 // send message
 $('#btn-send').on('click', function (e) {
     e.preventDefault();
-    var message = $('#message');
-    if (message.val() == '') {
-        return false;
-    }
-    message = message.val()
-    _handleChatBox(message, 'ME');
+    var message = $('#message').val();
+    if (message == '') {return false;}
+    _handleChatBox(message, key_chat, 'ME');
     $.ajax({
         url: base_url+ "api/send_message",
         headers: { 'X-CSRF-Token': $('meta[name="csrf-token"]').attr('content') },
         type: "POST",
         data: {
-            message,idp_owner
+            message,idp_owner,key_chat
         },
         dataType: "JSON",
         success: function (data) {
@@ -54,7 +53,8 @@ $('#btn-send').on('click', function (e) {
         }
     });
 });
-const _handleChatBox = (message, category) => {
+// handle chatbox response
+const _handleChatBox = (message, dt_keychat, category) => {
     let position = '';
     if(category == 'ME'){
         position = 'user';
@@ -66,8 +66,10 @@ const _handleChatBox = (message, category) => {
     var chatEmojiArea = $('.append-media').html();
             
     var current = new Date();
-    var ampm = current.getHours() >= 12 ? 'pm' : 'am';
-    var actualTime = (current.getHours()% 12 +':'+current.getMinutes() +' '+ ampm);
+    var hours = current.getHours();
+    var minutes = current.getMinutes();
+    minutes = minutes < 10 ? '0' + minutes : minutes; // menambahkan 0 di depan menit jika kurang dari 10
+    var actualTime = hours + ':' + minutes + ' ';
     
     var messageEmojiHtml = '<div class="chat-content '+position+'">'+
         '<div class="message-item">'+
@@ -75,37 +77,32 @@ const _handleChatBox = (message, category) => {
             '<div class="message-time">'+actualTime+'</div>'+
         '</div>'+
     '</div>';
-        
     if(chatEmojiArea.length > 0){   
-        $('.chat-box-area').append(messageEmojiHtml);
+        $('#'+dt_keychat+'').append(messageEmojiHtml);
     }
-    
     var messageHtml = '<div class="chat-content '+position+'">'+
         '<div class="message-item">'+
             '<div class="bubble">'+chatMessageValue+'</div>'+
             '<div class="message-time">'+actualTime+'</div>'+
         '</div>'+
     '</div>';
-    
     if(chatMessageValue.length > 0){
-        var appendMessage = $('.chat-box-area').append(messageHtml);
+        var appendMessage = $('#'+dt_keychat+'').append(messageHtml);
     }
-    
-    //console.log(document.body.scrollHeight)
     window.scrollTo(0, document.body.scrollHeight);
     var clearChatInputE = $('.append-media').empty();    
 }
 // Realtime chat
 const realtimeChat = () => {
     // realtime online status
-    Pusher.logToConsole = true;
+    Pusher.logToConsole = false;
     var pusher = new Pusher('776e717ca68fa5373ae0', {
         cluster: 'ap1'
     });
     var channel = pusher.subscribe('chat-personal');
     channel.bind('App\\Events\\ChatPersonal', function(data) {
-        if(data.user != my_id){
-            _handleChatBox(data.message);
+        if (data.from_user !== parseInt(my_id)) {
+            _handleChatBox(data.message, data.key_chat, '');
         }
     });
 }
