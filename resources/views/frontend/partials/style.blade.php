e2996002556f0d79580309cdbf130001c5507dee
SetiaOnce <gedeyoga1126@gmail.com>
2024-02-16 14:20:01 +0700

    Update style.blade.php

--- resources/views/frontend/partials/style.blade.php
+++ resources/views/frontend/partials/style.blade.php
@@ -5,6 +5,7 @@
 <link href="https://fonts.googleapis.com/css2?family=Nunito+Sans:opsz,wght@6..12,200;6..12,300;6..12,400;6..12,500;6..12,600;6..12,700;6..12,800;6..12,900;6..12,1000&family=Poppins:wght@100;200;300;400;500;600;700;800;900&display=swap" rel="stylesheet">
 <!-- Stylesheets -->
 <link rel="stylesheet" class="main-css" type="text/css" href="{{ asset('dist/assets/css/style.css') }}">
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
 <!-- Base route -->
 <!--begin::Vendor Stylesheets(used for this page only)-->
 @if (isset($data['css']))
